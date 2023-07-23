058a915aee1c89f480c9d0cc50304ef60152246d
SChakraborty04 <shaanch04@yahoo.com>
2023-07-23 20:04:46 +0530

    Added datatypes constants, arrays, strings, type casting and average calculation

--- DataTypes/DataTypes.cpp
+++ DataTypes/DataTypes.cpp
@@ -52,3 +52,88 @@ int main() {
 	a += b;
 	std::cout << "The required integer is " << a << std::endl;
 }
+//use of const Qualifier instead of #define; it is used in regular variable declaration; it specify a type;scope encapsulation is enforced.
+//# const is not the same in c and cpp.
+// const int x;  CTE error or compile-time error (CTE)
+	// x = 9;   CTE error
+const int y = 10;
+
+//Array: It is a continuous block of memory.
+#include<iostream>
+const int AGELENGTH = 3; //use of const
+int age[AGELENGTH];
+float number[] = {29.9,68.2,77.3}; //auto dont work for arrays;array length may not be specified.
+int main() {
+	age[0] = 1 ;
+	age[1] = 2 ;
+	age[2] = 3 ;
+	std::cout << "The required address of array is " << age << std::endl; //prints the address of array.
+	//approach to print elements of array without loop
+	std::cout << "Age[0] = " << age[0] << std::endl;
+	std::cout << "Age[1] = " << age[1] << std::endl;
+	std::cout << "Age[2] = " << age[2] << std::endl;
+	std::cout << "Number[0] = " << number[0] << std::endl;
+	std::cout << "Number[1] = " << number[1] << std::endl;
+	std::cout << "Number[2] = " << number[2] << std::endl;
+	return 0;
+}
+
+//Strings in cpp is not a native type; It is an array of characters; std::string class comes with cpp standard library
+#include<iostream>
+#include<string>
+#include<cstring>
+
+char array_str1[25] = "Hello "; //char array is used to make code compatible with existing C code.
+char array_str2[] = "World !";
+
+std::string str1 = "How are ";
+std::string str2 = "you?";
+
+int main() {
+	strcat_s(array_str1,25, array_str2); //This updates array_str1
+	std::cout << array_str1 << std::endl;
+	std::cout << str1+str2 << std::endl; //For string class concatenation is simple by just adding an addition sign. Also both strings remain intact.
+}
+
+//Typecasting specify how to interprete a piece of data; useful for converting datatypes.
+
+#include<iostream>
+#include<cstdint>
+
+float flt = -1.54f;
+int32_t sgn;
+uint32_t usgn;
+
+int main() {
+	sgn = flt;
+	usgn = sgn;
+	std::cout << "Float = " << flt << std::endl;
+	std::cout << "Signed int = " << sgn << std::endl;
+	std::cout << "Unsigned int = " << usgn << std::endl;
+
+	//Conversion of fahrenheit to celcius
+
+	int fah = 100;
+	int cel;
+	cel = ((float)5 / 9.0) * (fah - 32); //forcefully converting 5/9 to float as if it is in int then the value would be zero.
+	std::cout << std::endl;
+	std::cout << "Fahrenheit = " << fah << std::endl;
+	std::cout << "Celcius    = " << cel << std::endl;
+
+	//using typecasting to get integer and decimal part of a float number seperately.
+	float height = 11.994;
+	std::cout <<"Given height is "<<height<< std::endl;
+	std::cout <<"Integer part is "<<(int)height<< std::endl;
+	std::cout <<"Decimal part is "<<height -(int)height<< std::endl;
+	return 0;
+}
+
+//Calculating floating average (Challenge)
+#include<iostream>
+float arr[] = { 1.2,3.4,7.9,2.3,7.1 };
+int main() {
+	float sum = arr[0] + arr[1] + arr[2] + arr[3] + arr[4];
+	float avg = sum / 5.0;
+	std::cout << "The required average is " << avg << std::endl;
+	return 0;
+}
